89a4ef7da983ab955ec2284aa368a114c25cefeb
Lee <calzy8@mail.missouri.edu>
2017-11-12 16:33:59 -0600

    added saveStudent function, getters, and setters

--- student.h
+++ student.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include "book.h"
+#include <fstream>
+
 
 using namespace std;
 
@@ -13,20 +15,65 @@ using namespace std;
 
 class Student {		
 	protected:
-		int id;
 		string name;
-		vector<Book> books;
+		int id;
 		double balance;
-		
+		vector<Book> books;
 	public:
-		Student(int, string);
+		Student();
+		Student(string, int);
 		void checkIn(Book);
 		void acctInfo();
 		void checkOut(string);
 		void checkOut(int);
+		void saveStudent();
+		string getName() const;
+		int getID() const;
+		void setName(string);
+		void setID(int);
+		void setBalance(double);
+		double getBalance() const;
+		vector<Book> getBooks() const;
+
+		Student operator=(const Student&) const;
 			
 };
-Student::Student(int num, string nm) {
+
+void Student::setName(string nm) {
+	name = nm;
+}
+void Student::setID(int num) {
+	id = num;
+}
+void Student::setBalance(double bal) {
+	balance = bal;
+}
+vector<Book> Student::getBooks() const{
+	return books;
+}
+int Student::getID() const{
+	return id;
+}
+double Student::getBalance() const{ 
+	return balance;
+}
+Student Student::operator=(const Student &st1) const {
+	Student tmp;
+	tmp.name = st1.getName();
+	tmp.id = st1.getID();
+	tmp.balance = st1.getBalance();
+	tmp.books = st1.getBooks();
+return tmp;
+}
+string Student::getName() const{
+	return name;
+}
+Student::Student() {
+	id = 0;
+	name = " ";
+	balance = 0;
+}
+Student::Student(string nm, int num) {
 	id = num;
 	name = nm;
 	balance = 0;
@@ -62,4 +109,14 @@ void Student::checkOut(int num) {
 			books.erase(books.begin() + i - 1);
 		}
 	}
+}
+void Student::saveStudent() {
+	ofstream sFile;
+	sFile.open("students.txt", ios_base::app);
+	sFile << name << '|' << id << '|' << balance << '|';
+	for(auto &i : books) {
+		sFile << i.getTitle() << ' ';
+	}
+	sFile << endl;
+	sFile.close();
 }
