05024fd6585c1357acf09d56f65c303ada48eb99
Lee <calzy8@mail.missouri.edu>
2017-12-03 10:11:19 -0600

    added function for login to make main more readable

--- libManage.cpp
+++ libManage.cpp
@@ -10,57 +10,44 @@
 using namespace std;
 
 void menu();
-
+void login();
 
 int main() {
 	srand(time(NULL));
 	int choice;
-	menu();
-again:	cin >> choice;
-	switch(choice) {
-		case 1:{
-			cout << "Please enter name: ";
-			string nm;
-			string tmpName;
-			Student st1;
-			string tmp;
-			cin.ignore();
-			getline(cin, nm);
-			ifstream sFile;
-			sFile.open("students.txt");
-			while(!sFile.eof()) {
-				getline(sFile, tmp, '|');
-				if(tmp == nm) {
-					st1.setName(nm);
-					getline(sFile, tmp, '|');
-					st1.setID(stoi(tmp));
-					getline(sFile, tmp, '|');
-					st1.setBalance(stoi(tmp));
-				}
-				getline(sFile, tmp);
-			}
-			st1.acctInfo();
-			break;
-			}
-		case 2:
+	do {
+		menu();
+again:		cin >> choice;
+		switch(choice) {
+			case 1:	
 			{
-			string nm;
-			cin.ignore();
-			cout << "Please enter name: ";
-			getline(cin, nm);
-			Student st1(nm, (int)(rand() % 500 + 1));
-			st1.acctInfo();
-			st1.saveStudent();
-			break;
+				login();	
+				break;
 			}
-		case 3:
-			cout << "Exiting..." << endl;
-			break;
-		case 4:
-			cout << "Invalid option, please enter again" << endl;
-			goto again;
-	}
-	cout << endl;
+			case 2:
+			{
+				int num;
+				string nm;
+				cin.ignore();
+				cout << "Please Scan ID ";
+				cin >> num;
+				cin.ignore();
+				cout << "Please enter name: ";
+				getline(cin, nm);
+				Student st1(nm, num);
+				st1.acctInfo();
+				st1.saveStudent();
+				break;
+			}
+			case 3:
+				cout << "Exiting..." << endl;
+				break;
+			default:
+				cout << "Invalid option, please enter again" << endl;
+				goto again;
+		}
+		cout << endl;
+	} while(choice != 3);
 	return 0;	
 }
 void menu() {
@@ -68,5 +55,28 @@ void menu() {
 	cout << "Please chooce an option" << endl;
 	cout << "(1) Login" << endl;
 	cout << "(2) Register" << endl;
-	cout << "(0) Exit" << endl;
+	cout << "(3) Exit" << endl;
+}
+void login() {
+	cout << "Please Scan ID: ";
+	int num;
+	Student st1;
+	string tmp;
+	cin.ignore();
+	cin >> num;
+	ifstream sFile;
+	sFile.open("students.txt");
+	while(!sFile.eof()) {
+		getline(sFile, tmp, '|');
+		if(atoi(tmp.c_str()) == num) {
+			st1.setID(num);
+			getline(sFile, tmp, '|');
+			st1.setName(tmp);
+			getline(sFile, tmp, '|');
+			st1.setBalance(stoi(tmp));
+		}
+		getline(sFile, tmp);
+		
+	}
+	
 }
