e7197d6e3f415005fa513d01abf0a007d1490d51
Lee <calzy8@mail.missouri.edu>
2017-12-07 07:35:45 -0600

    updated

--- catalog.h
+++ catalog.h
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include "student.h"
 #include <cstdlib>
 #include <ctime>
 #include <string>
@@ -11,30 +10,36 @@ using namespace std;
 
 class Catalog {
 	protected:
-		vector<Book> books;
+		vector<Book> books;	//list of all books from catalog
 public:
 	Catalog();
-	Catalog(string);
+	Catalog(string);	//constructors
 	Book checkOut(string);
 	Book checkOut(int);
-	void checkIn(Book);
-	void display(void);
-
+	void checkIn(int);
+	void checkIn(string);
+	void display(void);	//display all books in catalog
+	bool bookExists(int);	//check that books are in the catalog
+	bool bookExists(string);
+	vector<Book> getBooks() const;
 };
+vector<Book> Catalog::getBooks() const {
+	return books;
+}
 Catalog::Catalog()
 {
-	Catalog("catalog.txt");
+	Catalog("catalog.txt");	//default
 }
 
 Catalog::Catalog(string booklist)
 {
 	string line;
-	string nm;
-	string auth;
-	int num;
+	string nm;	//title of book
+	string auth;	//author
+	int num;	//book number
 
 	ifstream catalog_file(booklist);
-	while (!catalog_file.eof())
+	while (!catalog_file.eof())	
 	{
 		getline(catalog_file, line, '|');
 		nm = line;
@@ -42,22 +47,23 @@ Catalog::Catalog(string booklist)
 		auth = line;
 		getline(catalog_file, line);
 		num = atoi(line.c_str());
-		Book tmp(nm, auth, num);
-		books.push_back(tmp);
-	}
+		Book tmp(nm, auth, num);	//make a tmp book
+		books.push_back(tmp);		//push tmp book to catalog
+	}	
+	catalog_file.cloose();
 }
 Book Catalog::checkOut(int num) {
 	int i;
 	for (i = 0; i < books.size(); i++) {
-		if (books[i].getNumber() == num) {
+		if (books[i].getNumber() == num) {	//loop through book vector
 			if (books[i].checkedOut)
 			{
-				return Book("NotAvailable","None", 0);
+				return Book("NotAvailable","None", 0);	//if checked out
 			}
 			else
 			{
 				books[i].checkedOut = true;
-				return Book(books[i].getTitle(), books[i].getAuthor(), books[i].getNumber());
+				return books[i];
 			}
 		}
 	}
@@ -73,27 +79,59 @@ Book Catalog::checkOut(string title) {
 			else
 			{
 				books[i].checkedOut = true;
-				return Book(books[i].getTitle(), books[i].getAuthor(), books[i].getNumber());
+				return books[i];
 			}
 		}
 	}
 }
-void Catalog::checkIn(Book b1)
+
+
+void Catalog::checkIn(int numberID)
 {
 	int i;
 	for (i = 0; i < books.size(); i++) {
-		if (books[i].getNumber() == b1.getNumber()) {
+		if (books[i].getNumber() == numberID) {
 			books[i].checkedOut = false;
 		}
 	}
 }
+
+void Catalog::checkIn(string title)
+{
+	int i;
+	for (i = 0; i < books.size(); i++) {
+		if (books[i].getTitle() == title) {
+			books[i].checkedOut = false;
+		}
+	}
+}
+
 void Catalog::display()
 {
 	int i;
 	for (i = 0; i < books.size(); i++)
 	{
-		books[i].display();
+		books[i].display();	//loop through calling display on all
 	}
 }
 
-
+bool Catalog::bookExists(int idNumber)
+{
+	int i;
+	for (i = 0; i < books.size(); i++) {
+		if (books[i].getNumber() == idNumber) { 	//loop through and seach for book title
+			return true;	//found
+		}
+	}
+	return false;		//doesn't exist
+}
+bool Catalog::bookExists(string titlecheck)
+{
+	int i;
+	for (i = 0; i < books.size(); i++) {
+		if (books[i].getTitle() == titlecheck) {
+			return true;
+		}
+	}
+	return false;
+}
