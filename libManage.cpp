8fa2764a7c7a31c0a6f77ee67ff4e4492beba22b
Lee <calzy8@mail.missouri.edu>
2017-12-07 07:36:09 -0600

    fixed A LOT of errors, added check in and check out

--- libManage.cpp
+++ libManage.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
-#include "student.h"
+#include "std.h"
+#include "catalog.h"
 #include <cstdlib>
 #include <ctime>
 #include <string>
@@ -10,22 +11,14 @@
 using namespace std;
 
 void mainMenu();				//display menu
-void studentOptions(Student, vector<Book>);	//student menu
-void login(vector<Book>);			//login student
-void reg(vector<Book>);					//register student
-vector<Book> getBooks();			//list of available books
-void updateCatalog(vector<Book>);
+void studentOptions(Student, Catalog);	//student menu
+void login(Catalog);			//login student
+void reg(Catalog);					//register student
+void updateCatalog(Catalog);
 
 
 int main() {
-	srand(time(NULL));
-	try {
-		vector<Book> books = getBooks();	//list of Books
-	} catch (const char* feo){
-		cout << "Can't open catalog.txt, make sure you have it and try again" << endl;
-		return 0;
-	}
-	/*
+	Catalog books("catalog.txt");	
 	int choice;
 	do {
 		mainMenu();
@@ -42,7 +35,7 @@ again:		if(!(cin >> choice)){
 					login(books);	
 					break;
 				} catch (string feo) {
-					cout << "Can't open students.txt, please make sure you have it and try again" << endl;
+					cout << "Can't open students.txt, please make sure you have it and try again" << endl; //if students cant be opened, exit program since it would break
 					return 0;
 				}
 			}
@@ -65,7 +58,6 @@ again:		if(!(cin >> choice)){
 		}
 		cout << endl;
 	} while(choice != 3);
-*/
 	return 0;	
 }
 void mainMenu() {
@@ -75,7 +67,7 @@ void mainMenu() {
 	cout << "(2) Register" << endl;
 	cout << "(3) Exit" << endl;
 }
-void login(vector<Book> catalog) {
+void login(Catalog c1) {
 	ifstream sFile;
 	sFile.open("students.txt");
 	if(!(sFile.is_open())) {
@@ -86,63 +78,48 @@ void login(vector<Book> catalog) {
 	int num;
 	Student st1;	//used to get student information and store it in a local variable
 	string tmp;	//to pull information from file
+	string tmp2;
 	cin.ignore();
 	cin >> num;
-	while(!sFile.eof()) {
-		getline(sFile, tmp, '|');	//look at ID of student
-		if(atoi(tmp.c_str()) == num) {	//if matched
+	while(getline(sFile, tmp, '|')) {
+		tmp2 = tmp;
+		if(atoi(tmp2.c_str()) == num) {	//if matched
 			cout << endl;
 			st1.setID(num);
 			getline(sFile, tmp, '|');		//pull student information from file
 			st1.setName(tmp);
-			getline(sFile, tmp, '|');
-			st1.setBalance(stoi(tmp));
-			studentOptions(st1, catalog);
+			while(getline(sFile, tmp, ' ')) {
+				if(tmp == "\n") {
+					break;
+				}
+				for(auto &i : c1.getBooks()) {	//loop through file and pull book numbers and search for them in catalog
+					if(i.getNumber() == atoi(tmp.c_str())){
+						st1.checkOut(c1.checkOut(atoi(tmp.c_str())));	//append books to students checked out books
+						break;
+					}
+				}
+			}
+			studentOptions(st1, c1);
 			sFile.close();
-			return;
-			
+			return;	
 		}
 		getline(sFile, tmp);	
-	}
-	cout << "Student Number not found" << endl;
+	}	
+	cout << "Student Number not found" << endl;	//if reach eof, student not found
 	sFile.close();
 }
-vector<Book> getBooks(){
-	ifstream catalog_file("catalog.txt");
-	if(!(catalog_file.is_open())) {
-		throw "Can't open file";
-	} 
-	vector<Book> catalog;		//list of books
-	
-	string line;
-	string nm;	//book number
-	string auth;	//author
-	int num;
-	while(!catalog_file.eof())
-	{
-		getline(catalog_file, line, '|');
-		nm = line;
-		getline(catalog_file, line, '|');
-		auth = line;				//reads in information from file about book and stores it in a Book object
-		getline(catalog_file, line);
-		num = atoi(line.c_str());
-		Book tmp(nm, auth, num);
-		catalog.push_back(tmp);	//add book to list
-	}
-	catalog.pop_back();
-	catalog_file.close();
-	return catalog;
-}
-void studentOptions(Student std, vector<Book> catalog) {
+void studentOptions(Student std, Catalog c1) {
 	int choice;
 	do {
 		cout << "Please choose an option" << endl;
 		cout << "1. View Account Information" << endl;
 		cout << "2. View Catalog" << endl;
 		cout << "3. Search Catalog" << endl;
-		cout << "3. Check In Book" << endl;
-		cout << "4. Check Out Book" << endl;
-		cout << "5. Exit" << endl;
+		cout << "4. Check in book by title" << endl;
+		cout << "5. Check in book by number" << endl;
+		cout << "6. Check out book by title" << endl;
+		cout << "7. Check out book by number" << endl;
+		cout << "8. Exit" << endl;
 inv:		if(!(cin >> choice)) {
 			cout << "Invalid input, please enter a valid option: ";	//if user enters a non-integer
 			cin.clear();
@@ -157,10 +134,7 @@ inv:		if(!(cin >> choice)) {
 				cout << endl;
 				break;
 			case 2:
-				for(auto &c : catalog) {	//range for to loop through cataglog and display the book info
-					c.display();
-				}
-				cout << endl;
+				c1.display();
 				break;
 			case 3:
 			{
@@ -195,30 +169,91 @@ inv:		if(!(cin >> choice)) {
 				break;
 			}
 			case 4:
-				break;
+				{
+					string booktitle;
+					cout << "What is the book title you want to check in?  ";
+					cin.ignore();
+					getline(cin,booktitle);
+					if (c1.bookExists(booktitle))	//check book exists
+					{
+						std.checkIn(booktitle);
+						c1.checkIn(booktitle);
+						std.updateStudent();	//update to rewrite
+					}
+					else
+					{
+						cout << "This book is not contained within our catalog";	//book not found
+					}
+					break;
+				}
+			
 			case 5:
+				{
+					int booknumber;
+					cout << "What is the book number you want to check in?  ";
+					cin >> booknumber;
+					if (c1.bookExists(booknumber))
+					{
+						std.checkIn(booknumber);
+						c1.checkIn(booknumber);		//same as before but with number
+						std.updateStudent();
+					}
+					else
+					{
+						cout << "This book is not contained within our catalog";
+					}
+					break;
+				}
+
+				break;
+			case 6:
+				{
+					string booktitle;
+					cout << "What is the book title you want to check out?  ";
+					cin.ignore();
+					getline(cin,booktitle);
+	
+					if (c1.bookExists(booktitle))	//check book exists
+					{
+						std.checkOut(c1.checkOut(booktitle));	//checkout and update student	
+						std.updateStudent();
+					}
+					else
+					{
+						cout << "Sorry that book doesn't exist in the catalog." << endl;
+					}
+
+					break;
+				}
+
+				break;
+			case 7:
+			{
+					int booknumber;
+					cout << "What is the book number you want to check out?  ";
+					cin >> booknumber;
+					if (c1.bookExists(booknumber))
+					{
+						std.checkOut(c1.checkOut(booknumber));	//same as above but with number
+					}
+					else
+					{
+						cout << "Sorry that book doesn't exist in the catalog." << endl;
+					}
+					break;
+			}
+
+				break;
+			case 8:
 				break;	//break the loop, exit the menu
 			default:
 				cout << "Invalid input, please enter a valid option: ";	//numerical invalid input
 				goto inv;
 			
 		}
-	}while(choice != 5);
+	}while(choice != 8);
 }
-void updateCatalog(vector<Book> books) {
-	ofstream cf;
-	cf.open("catalog.txt");
-	if(cf.is_open()){
-		for(auto &c : books) {
-			cf << c.getTitle() << '|' << c.getAuthor() << '|' << c.getNumber() << endl; //rewrite the catalog file	
-		}
-		cf.close();
-	} else {
-		throw "Can't open file";
-	}
-	
-}
-void reg(vector<Book> catalog) {
+void reg(Catalog c1){
 	ifstream sFile;
 	sFile.open("students.txt");
 	if(!sFile.is_open()) {
@@ -246,8 +281,6 @@ void reg(vector<Book> catalog) {
 	cout << "Please enter name: ";
 	getline(cin, nm);
 	Student st1(nm, num);
-	st1.acctInfo();
-	st1.saveStudent();	//save student to file
-	studentOptions(st1, catalog);
-
+	st1.saveStudent();
+	studentOptions(st1, c1);
 }
